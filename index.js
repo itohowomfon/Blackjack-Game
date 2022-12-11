cac2d458ce94f57811dd7928e4c4ac4197f5c240
Mfon Itohowo <mfonation29@gmail.com>
2022-12-11 20:49:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+//console.log(messageEl);
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]//firstCard + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+        //console.log("Do you want to draw a new card?");
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    console.log("Drawing out a new card from the deck!");
+    let card = 7
+    sum += card
+    cards.push(card)
+    console.log(cards);
+    renderGame()
+} 
+
